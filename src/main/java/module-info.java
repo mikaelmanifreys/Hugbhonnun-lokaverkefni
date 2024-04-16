67086a42ef2653c76f86d9645fb988d708008c41
mikaelmanifreys <mikaelmanifreys98@gmail.com>
2024-04-16 21:48:48 +0000

    README file added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module com.example.audioplayer {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
-    requires junit;
-    requires org.junit.jupiter.api;
 
 
     opens com.example.audioplayer to javafx.fxml;
